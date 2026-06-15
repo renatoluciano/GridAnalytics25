b9f2ca6e330c771e2e669f31b2feacbb7a38f7a8
Rento Dutra Luciano <renatoluciano@gmail.com>
2026-06-15 12:31:19 -0300

    Feat: Criado o app analisador e registrado no settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Nosso app adicionado aqui:
+    'analisador',
 ]
 
 MIDDLEWARE = [
